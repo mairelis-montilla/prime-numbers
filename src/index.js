ebf147864a7319e88e32044b470eef5838374986
maire-montilla <mairelismontilla@gmail.com>
2022-08-06 17:13:33 -0500

    feat print in console prime numbers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+const { Command } = require('commander');
+const { getPrimeNumbers } = require('./methods');
 
-console.log('start to code')
+const program = new Command();
+
+program
+  .requiredOption('-f, --firt <number>', 'must have first number')
+  .requiredOption('-l, --last <number>', 'must have last number')
+  .action((options) => {
+    const firstNumber = Number(options.firt);
+    const lastNumber = Number(options.last);
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(firstNumber) || isNaN(lastNumber)) {
+      console.log('first and last need to be a number');
+      return;
+    }
+    if (firstNumber === lastNumber || firstNumber > lastNumber) {
+      console.log('first number need to be greather than last number');
+      return;
+    }
+    const primeNumbers = getPrimeNumbers(firstNumber, lastNumber);
+    console.log(primeNumbers.join(', '));
+  });
+program.parse();
