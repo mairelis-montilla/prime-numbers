c0e427a49591831bcfe03b555d17930e025c9711
maire-montilla <mairelismontilla@gmail.com>
2022-08-08 01:47:18 -0500

    docs readme

--- src/index.js
+++ src/index.js
@@ -6,18 +6,22 @@ const { getPrimeNumbers } = require('./methods');
 const program = new Command();
 
 program
-  .requiredOption('-f, --firt <number>', 'must have first number')
+  .requiredOption('-f, --first <number>', 'must have first number')
   .requiredOption('-l, --last <number>', 'must have last number')
   .action((options) => {
-    const firstNumber = Number(options.firt);
+    const firstNumber = Number(options.first);
     const lastNumber = Number(options.last);
     // eslint-disable-next-line no-restricted-globals
     if (isNaN(firstNumber) || isNaN(lastNumber)) {
       console.log('first and last need to be a number');
       return;
     }
-    if (firstNumber === lastNumber || firstNumber > lastNumber) {
+    if (firstNumber >= lastNumber) {
       console.log('first number need to be greather than last number');
+      return;
+    }
+    if (firstNumber <= 0 || lastNumber <= 0) {
+      console.log('first number and last number greather than 0');
       return;
     }
     const primeNumbers = getPrimeNumbers(firstNumber, lastNumber);
--- src/methods/index.js
+++ src/methods/index.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-plusplus */
 const isPrime = (n) => {
-  if (n <= 1) return false;
-  for (let i = 2; i <= n - 1; i++) { if (n % i === 0) return false; }
+  if (n <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= n - 1; i++) {
+    if (n % i === 0) return false;
+  }
   return true;
 };
 
